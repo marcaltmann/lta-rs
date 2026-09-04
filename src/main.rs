a9702123afe49534b32ca666216d6643a9d31170
Marc Altmann <marcaltmann@posteo.de>
2026-09-04 13:40:43 +0200

    Extract get_config function

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,30 @@
 use clap::{Command, Arg};
 
+struct Config {
+    domain: String,
+    target_dir: String,
+}
+
 fn main() {
+    let config: Config = get_config();
+
+    println!("{}", config.domain);
+    println!("{}", config.target_dir);
+}
+
+fn get_config() -> Config {
     let matches = Command::new("lta-rs")
         .version("1.0")
         .about("Long term archiving tool")
         .arg(Arg::new("domain").short('d').long("domain").required(true))
-        .arg(Arg::new("target").short('t').long("target").required(true))
+        .arg(Arg::new("target").short('t').long("target-dir").required(true))
         .get_matches();
 
     let domain: &String = matches.get_one::<String>("domain").unwrap();
-    println!("{}", domain);
     let target_dir: &String = matches.get_one::<String>("target").unwrap();
-    println!("{}", target_dir);
+
+    return Config {
+        domain: String::from(domain),
+        target_dir: String::from(target_dir),
+    };
 }
