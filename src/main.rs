cbc16d748321ab414387e6613efd69d94a5db635
Marc Altmann <marcaltmann@posteo.de>
2026-09-04 20:07:37 +0200

    Work on API

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,42 @@
-use clap::{Command, Arg};
+use clap::{Command, Arg, ArgAction};
 
 struct Config {
-    domain: String,
-    target_dir: String,
+    config_file: String,
+    force: bool,
 }
 
 fn main() {
     let config: Config = get_config();
 
-    println!("{}", config.domain);
-    println!("{}", config.target_dir);
+    println!("{}", config.config_file);
+    println!("{}", config.force);
 }
 
 fn get_config() -> Config {
     let matches = Command::new("lta-rs")
+        .author("Marc Altmann, marcaltmann@posteo.de")
         .version("1.0")
         .about("Long term archiving tool")
-        .arg(Arg::new("domain").short('d').long("domain").required(true))
-        .arg(Arg::new("target").short('t').long("target-dir").required(true))
+        .arg(
+            Arg::new("config_file")
+                .required(true)
+        )
+        .arg(
+            Arg::new("force")
+                .short('f')
+                .long("force")
+                .help("Always fetch CMDI files")
+                .action(ArgAction::SetTrue)
+        )
+        .after_help("Point the tool to the yaml configuration file \
+                     in the batch directory.")
         .get_matches();
 
-    let domain: &String = matches.get_one::<String>("domain").unwrap();
-    let target_dir: &String = matches.get_one::<String>("target").unwrap();
+    let config_file: &String = matches.get_one::<String>("config_file").unwrap();
+    let force: &bool = matches.get_one::<bool>("force").unwrap();
 
     return Config {
-        domain: String::from(domain),
-        target_dir: String::from(target_dir),
+        config_file: String::from(config_file),
+        force: *force,
     };
 }
