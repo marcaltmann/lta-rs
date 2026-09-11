e39e8507d6ee3924770669f0d2ddf79d5f1c8c87
Marc Altmann <marcaltmann@posteo.de>
2026-09-11 10:25:50 +0200

    Extract function for single directory

--- src/checks.rs
+++ src/checks.rs
@@ -6,21 +6,32 @@ pub fn check_session_dirs(paths: &Vec<PathBuf>) -> Vec<Error> {
 	let mut errors: Vec<Error> = Vec::new();
 
 	for path in paths {
-		if !fs::exists(path).unwrap() {
-			let message = format!("{} not found", path.display());
-			let error = Error::new(ErrorKind::NotFound, message);
-			errors.push(error);
-			continue;
-		}
+		let errors_for_round: Vec<Error> = check_directory(path.to_path_buf());
 
-		let metadata = path.metadata().unwrap();
-
-		if !metadata.is_dir() {
-			let message = format!("{} is no directory", path.display());
-			let error = Error::new(ErrorKind::NotADirectory, message);
+		for error in errors_for_round {
 			errors.push(error);
-			continue;
 		}
+	}
+
+	errors
+}
+
+pub fn check_directory(path: PathBuf) -> Vec<Error> {
+	let mut errors: Vec<Error> = Vec::new();
+
+	if !fs::exists(&path).unwrap() {
+		let message = format!("{} not found", path.display());
+		let error = Error::new(ErrorKind::NotFound, message);
+		errors.push(error);
+		return errors
+	}
+
+	let metadata = path.metadata().unwrap();
+
+	if !metadata.is_dir() {
+		let message = format!("{} is no directory", path.display());
+		let error = Error::new(ErrorKind::NotADirectory, message);
+		errors.push(error);
 	}
 
 	errors
